b32c5091dac91d5d92842ef19a2d6ea6df095b8d
edersonlrf <edersonluis.rf@gmail.com>
2018-04-04 19:54:52 -0300

    Aula02 exercicio 02

---
+++ Aula02/exercicio02.cpp
@@ -0,0 +1,26 @@
+/*
+2 - Criar um programa que leia um caracter e um string do teclado, e informe se o
+caracter está contido no texto lido.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int main(void) {
+	char s1[80], s2;
+	int i;
+	printf("Ler string: \n");
+	gets(s1);
+	printf("Ler caracter: \n");
+	scanf("%c",&s2);
+
+	for(i=0; i < strlen(s1); i++) {
+		if (s1[i] == s2) {
+			printf("%c está contido na posição %d em %s \n", s2, i, s1);
+		}
+	}
+	system("pause");
+}
+
